090ace0cc5b7cb0a58f0c348f4a8c25840adb2cc
Luigi Salvatore Palese <luigisalvatorepalese@gmail.com>
2020-09-01 08:37:46 +0200

    Fix compilation error in parse fun

--- lib/rethinkdb/pseudotypes.ex
+++ lib/rethinkdb/pseudotypes.ex
@@ -58,45 +58,36 @@ defmodule RethinkDB.Pseudotypes do
           %__MODULE__{epoch_time: epoch_time, timezone: timezone}
 
         _ ->
-          {seconds, ""} = Calendar.ISO.parse_offset(timezone)
+          with <<sign::binary-size(1)>> <> rest = timezone <> ":00",
+               {:ok, {h, m, _, _}} = Calendar.ISO.parse_time(rest) do
+            sec = (h * 60 + m) * 60
 
-          zone_abbr =
-            case seconds do
-              0 -> "UTC"
-              _ -> timezone
-            end
+            zone_abbr =
+              case sec do
+                0 -> "UTC"
+                _ -> timezone
+              end
 
-          negative = seconds < 0
-          seconds = abs(seconds)
+            time_zone =
+              case {div(sec, 3600), rem(sec, 3600)} do
+                {0, 0} ->
+                  "Etc/UTC"
 
-          time_zone =
-            case {div(seconds, 3600), rem(seconds, 3600)} do
-              {0, 0} ->
-                "Etc/UTC"
+                {hours, 0} ->
+                  "Etc/GMT" <> sign <> Integer.to_string(hours)
 
-              {hours, 0} ->
-                "Etc/GMT" <>
-                  if negative do
-                    "+"
-                  else
-                    "-"
-                  end <> Integer.to_string(hours)
+                {hours, seconds} ->
+                  "Etc/GMT" <>
+                    sign <>
+                    Integer.to_string(hours) <>
+                    ":" <> String.pad_leading(Integer.to_string(seconds), 2, "0")
+              end
 
-              {hours, seconds} ->
-                "Etc/GMT" <>
-                  if negative do
-                    "+"
-                  else
-                    "-"
-                  end <>
-                  Integer.to_string(hours) <>
-                  ":" <> String.pad_leading(Integer.to_string(seconds), 2, "0")
-            end
-
-          (epoch_time * 1000)
-          |> trunc()
-          |> DateTime.from_unix!(:milliseconds)
-          |> struct(utc_offset: seconds, zone_abbr: zone_abbr, time_zone: time_zone)
+            (epoch_time * 1000)
+            |> trunc
+            |> DateTime.from_unix!(:millisecond)
+            |> struct(utc_offset: sec, zone_abbr: zone_abbr, time_zone: time_zone)
+          end
       end
     end
   end
